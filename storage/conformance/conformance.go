4f9b7bf1230f427d18c42d90acb6e1538ec0475a
Mangaal <angommeeteimangaal@gmail.com>
2026-05-04 15:22:27 +0530

    fix lint issue
    Signed-off-by: Mangaal <angommeeteimangaal@gmail.com>

--- storage/conformance/conformance.go
+++ storage/conformance/conformance.go
@@ -508,7 +508,7 @@ func testPasswordCRUD(t *testing.T, s storage.Storage) {
 	password1.Username = "jane doe"
 	getAndCompare("jane@example.com", password1)
 
-	var passwordList []storage.Password
+	passwordList := make([]storage.Password, 0, 2)
 	passwordList = append(passwordList, password1, password2)
 
 	listAndCompare := func(want []storage.Password) {
